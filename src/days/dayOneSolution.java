dbc1f8b151154c5aae69989c77050bcda00066f4
PetrEconomist <popeter@seznam.cz>
2020-12-01 19:38:36 +0100

    day 1 part 2 before comments

--- src/days/dayOneSolution.java
+++ src/days/dayOneSolution.java
@@ -9,15 +9,19 @@ public class dayOneSolution {
 	
 	public static void main(String[] args) {
 		int expectedSum = 2020;
-		int[] pair = getPairSumEqual("dayOneInput.txt", expectedSum);
+		LinkedList<Integer> input = convertStringToIntList(ReadFile.getFileAsList("dayOneInput.txt"));
+		int[] pair = getPairSumEqual(input, expectedSum);
 		int result = pair[0]*pair[1];
 		System.out.printf("Answer, day 1 part 1 %d\n", result);
+		
+		int[] triplet = getTripleSumEqual(input, expectedSum);
+		int result2 = triplet[0]*triplet[1]*triplet[2];
+		System.out.printf("Answer, day 1 part 2 %d\n", result2);
 
 	}
 	
-	public static int[] getPairSumEqual(String fileName, int expectedSum) {
+	public static int[] getPairSumEqual(LinkedList<Integer> input, int expectedSum) {
 		int[] pair = new int[2];
-		LinkedList<Integer> input = convertStringToIntList(ReadFile.getFileAsList(fileName));
 		for(int i=0;i<input.size();i++) {
 			int firstVal = input.get(i);
 			for(int j=i+1;j<input.size();j++) {
@@ -29,8 +33,33 @@ public class dayOneSolution {
 				}
 			}
 		}
+		return null;
+	}
 		
-		return pair;
+	public static int[] getTripleSumEqual(LinkedList<Integer> input, int expectedSum) {
+	int[] triplet = new int[3];
+	int[] remainderPair = new int[2];
+	for(int i=0;i<input.size();i++) {
+		int err = 1827;
+		if(i==14) {
+			System.out.printf("here is some error\n");
+		}
+		int remainder = expectedSum-input.get(i);
+		LinkedList<Integer> remainderInput = (LinkedList<Integer>) input.clone();
+		remainderInput.remove(i);
+		remainderPair = getPairSumEqual(remainderInput, remainder);
+		if(remainderPair!=null) {
+			triplet[0] = input.get(i);
+			triplet[1] = remainderPair[0];
+			triplet[2] = remainderPair[1];
+			if(triplet[0]==err) {
+				System.out.printf("here is some error\n");
+			}
+			System.out.printf("Found solution %d & %d & %d\n", triplet[0], triplet[1], triplet[2] );
+			return triplet;
+		}
+	}
+		return null;
 	}
 	
 	public static LinkedList<Integer> convertStringToIntList(LinkedList<String> input){
